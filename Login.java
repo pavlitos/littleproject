d711a2cde519b371fcb589efb55cc321213ba5c0
Gleventis <43603733+Gleventis@users.noreply.github.com>
2018-10-11 21:35:39 +0200

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Login {
 	
 	//Για να γλιτωσουμε το database χρησιμοποιουμε ArrayList οπου θα εχει τα attributes των employees
-	private ArrayList<Employee> employees;
+	private List<Employee> employees;
+	String position[] = new String[12];
 	
-	public Login() {
-		
+	public Login() {		
 		employees = new ArrayList<>();
+
+//		String position[] = new String[employees.size()];
+
 		
 		//Customer Service
 		employees.add(new Employee("Sarah", "111", "Customer Service", "sarah@sep.se", 20, "female", 1));
@@ -32,18 +36,46 @@ public class Login {
 		
 		//HR 
 		employees.add(new Employee("Simon", "678", "HR", "simon@sep.se", 35, "male", 10));
-		
-
-		}
+	}
 	
-	public Employee EmployeeLogin(String name, String password) {
-		
+	
+	public Employee EmployeeLogin(String name, String password) {		
 		for(Employee employee : employees) {
-			if(employee.getName().equals(name) && employee.getPassword().equals(password))
+			if(employee.getName().equals(name) && employee.getPassword().equals(password)) {
+				System.out.println("You have logged in succesfully!");
 				return employee;
-			}
-	
+				}
+		}
 		return null;
 
 	}
+	
+	public String[] position() {
+		int i = 0;
+		for(Employee employee : employees) {
+			//System.out.println(employee.getPosition());
+			position[i] = employee.getPosition();
+			//System.out.println(position[i]);
+			i++;
+		}
+		return position;
+	}
+
+	
+	public List<Employee> getEmployee(){
+		return employees;
+	}
+	
+
+		
 }
+	
+
+
+
+	
+	
+
+	
+	
+
